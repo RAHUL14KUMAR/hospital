124d6d49c7b7e04e818213a251adf41d9cdbadaf
rahul14kumar <rahul.kumar.it25@heritageit.edu.in>
2024-02-12 22:34:48 +0530

    add some changes

--- src/component2/Contact/Contact.js
+++ src/component2/Contact/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
   return (
     <div className="my-auto bg-emerald-100 p-5 h-screen">
         <div className="my-12 flex flex-col p-5 md:flex-row justify-evenly">
-            <motion.a href="agrawal.r1412@gmail.com" target='_blank' whileHover={{scale:1.2}} className="cursor-pointer m-5 mx-auto flex-col items-center justify-center bg-white p-5 text-center shadow-2xl">
+            <motion.a whileHover={{scale:1.2}} className="cursor-pointer m-5 mx-auto flex-col items-center justify-center bg-white p-5 text-center shadow-2xl">
                 <div className="text-embg-emerald-900 mx-auto my-auto flex h-24 w-24 items-center justify-center bg-emerald-200 font-bold">
                     <BsMailbox2 className="text-4xl font-bold text-emerald-900"/>
                 </div>
